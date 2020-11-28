140ef2e5871655ad69966266723c5b8f829dda33
lucaskrespi <lucas.krespi@gmail.com>
2020-11-28 10:21:19 -0500

    DoublyLinkedList created

--- GAME2001_A2_KrespiLucas/GAME2001_A2_KrespiLucas/Source.cpp
+++ GAME2001_A2_KrespiLucas/GAME2001_A2_KrespiLucas/Source.cpp
@@ -1,10 +1,29 @@
-#include "SinglyLinkedList.h"
 #include <iostream>
+#include "DoublyLinkedList.h"
 
 
 int main() {
+	std::cout << "TEST 01" << std::endl;
 
-	std::cout << "Test-01" << std::endl;
+	LinkedList<int> Test;
+
+	Test.Push(1);
+	Test.Pop();
+	Test.Push(2);
+	Test.Push(3);
+	Test.Push(4);
+	Test.Push_Front(450);
+	Test.Push_Front(5);
+	Test.Pop_Front();
+
+	LinkIterator<int> it;
+
+	for (it = Test.Begin(); it != Test.End(); it++)
+	{
+		std::cout << " " << *it << std::endl;
+	}
+
+	
 
 	return 0;
 }
